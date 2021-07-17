0a1a3fd4adf5070b6c25097350f9e2c088348820
LvXiaoBu <49021590+uinio@users.noreply.github.com>
2021-07-17 20:42:48 +0800

    update: vite 2.4

---
+++ src/pages/App.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Button } from 'antd';
+import logo from '../assets/logo.svg';
+import styles from './app.module.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <Button onClick={() => setCount((counts) => counts + 1)}>
+            count is: {count}
+          </Button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className={styles.appLinkA}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className={styles.appLink}
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer">
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,28 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import reactRefresh from '@vitejs/plugin-react-refresh';
-import styleImport from 'vite-plugin-style-import';
+import ViteStyleImport from 'vite-plugin-style-import';
 
 export default defineConfig({
-  resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
-  },
-  plugins: [
-    reactRefresh(),
-    styleImport({
+  plugins: [reactRefresh(),
+    ViteStyleImport({
       libs: [
         {
           libraryName: 'antd',
           esModule: true,
-          resolveStyle: (name) => `antd/es/${name}/style/index`,
-        },
+          resolveStyle: (name) => {
+            return `antd/es/${name}/style/index`;
+          },
+        }
       ],
-    }),
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 80,
-    https: false,
-    open: false,
-    cors: true,
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://127.0.0.1:8080',
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, ''),
-    //   },
-    // },
+    })],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src'),
+      },
+    ],
   },
   css: {
     preprocessorOptions: {
@@ -48,5 +39,19 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+    https: false,
+    open: false,
+    cors: true,
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://127.0.0.1:8080',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ''),
+    //   },
+    // },
   },
 });
