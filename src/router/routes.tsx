8fd92c0532cf75de2fbe2a658e5dd0854450d482
LvXiaoBu <49021590+uinio@users.noreply.github.com>
2021-09-18 21:05:15 +0800

    update:vite 2.5

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { RouteConfig } from 'react-router-config';
+import { Redirect } from 'react-router-dom';
 import App from '@/pages/App';
 import HelloWord from '@/pages/HelloWord';
-import { Redirect } from 'react-router-dom';
 import NotFound from '@/pages/NotFound';
 
 const routes: RouteConfig[] = [
