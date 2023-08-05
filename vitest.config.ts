5a3bc087f0a1b2ae5d107f177e245d439d36f943
mioxs <49021590+uinio@users.noreply.github.com>
2023-08-05 20:30:53 +0800

    vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,4 @@
-import type { UserConfig } from 'vitest/config';
-import { mergeConfig, defineConfig } from 'vitest/config';
+import { defineConfig, mergeConfig } from 'vitest/config';
 import viteConfig from './vite.config';
 
 export default mergeConfig(
@@ -8,9 +7,9 @@ export default mergeConfig(
     test: {
       globals: true,
       environment: 'happy-dom',
-      transformMode: {
-        web: [/\.([tj]s|[tj]sx)$/],
+      testTransformMode: {
+        web: ['.ts', '.tsx', '.js', '.jsx'],
       },
     },
-  }) as UserConfig,
+  }),
 );
