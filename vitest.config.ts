6eb576f6e9c50ae9c4e906baa2e9146fe5e26cc7
mioxs <49021590+uinio@users.noreply.github.com>
2023-12-05 22:50:55 +0800

    update vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig, mergeConfig } from 'vitest/config';
 
 import viteConfig from './vite.config';
-
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
       globals: true,
       environment: 'happy-dom',
-      testTransformMode: {
-        web: ['.ts', '.tsx', '.js', '.jsx'],
-      },
     },
   }),
 );
